3e754c166cbe00e7056231c36d85a91470f66e40
Penguin <ad0097@uah.edu>
2020-07-18 13:10:22 -0500

    trying to fix things

---
+++ src/igloo.rs
@@ -0,0 +1,168 @@
+
+
+use std::vec::Vec;
+use std::string::String;
+use std::collections::HashMap;
+use config::*;
+
+use clap::{Arg, App};
+enum BuildType
+{
+	Release,
+	Debug,
+}
+// used for managing existing igloo projects
+// Igloo Project Manager - A struct for holding all
+// information pertaining to the current Igloo Project
+// An igloo project uses the .igloo folder to maintain
+// knowledge of its existence.
+pub struct ProjectManager
+{
+	
+}
+
+// used for managing cli requests for new and existing igloo projects
+pub struct CliManager<'b>
+{
+	app: clap::App<'b>,
+	debug_mode: bool,
+	release_mode: bool,
+	fresh_mode: bool,
+	version: String,
+	name: String,
+	author: &'b str,
+	description: &'b str,
+}
+
+impl<'b> CliManager<'b>
+{
+	pub fn new(this: &'b Self) -> Self
+	{
+		let _version = "v".to_owned() + crate_version!();
+		CliManager
+		{
+			version: _version.to_owned(),
+			name: crate_name!().to_owned(),
+			author: crate_authors!(),
+			description: crate_description!(),
+			debug_mode: true,
+			release_mode: false,
+			fresh_mode: false,
+			app: clap::App::new(this.name)
+				.author(this.author)
+				.version(&*this.version)
+				.about(this.description),
+
+		}
+	}
+}
+// Make Manifest contains default flags, files, and
+// include directories. User files, flags, and directories
+// can be added via the user yml in the cfg folder. They will be
+// appended to these lists which are read from the default make manifest
+// section in the mcu series yml file
+pub struct MakeManifest
+{
+	linker_script: String,
+	src_files: Vec<config::Value>,
+	inc_dirs: Vec<config::Value>,
+	cflags: Vec<config::Value>,
+	libs: Vec<config::Value>,
+	cc: String,
+	ld: String,
+	ar: String,
+}
+
+impl MakeManifest
+{
+	fn from_config(conf: &mut config::Config) -> MakeManifest
+	{
+		MakeManifest
+		{
+			src_files: conf.get_array("MAKEFILE_DEFAULT_SRC_FILES").unwrap_or_default(),
+			inc_dirs: conf.get_array("MAKEFILE_DEFAULT_INC_DIRS").unwrap_or_default(),
+			cflags: conf.get_array("CFLAGS").unwrap_or_default(),
+			libs: conf.get_array("EP_LIBS").unwrap_or_default(),
+			cc: conf.get_str("CC").unwrap_or_default(),
+			ld: conf.get_str("LD").unwrap_or_default(),
+			ar: conf.get_str("AR").unwrap_or_default(),
+			linker_script: conf.get_str("LINKER_SCRIPT").unwrap_or_default(),
+		}
+	}
+}
+impl std::fmt::Display for MakeManifest
+{
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result
+	{
+		// print src files
+		for src_file in &self.src_files
+		{
+			write!(f, "{}\n", src_file).unwrap();
+		}
+		// print inc dirs
+		// print cflags
+		// print cc
+		// print ld
+		// print ar
+		// print default linker script
+
+		Ok(())
+	}
+}
+
+// MCU Manifest contains the options for the mcu
+// If, for example, a MCU has a USART peripheral,
+// it is listed as a driver option here
+// The user project manifest's mcu options are compared to the
+// available options from the epsf which are stored here once read
+struct McuManifest
+{
+	core_deps: HashMap<String, config::Value>,
+	drivers: Vec<config::Value>,
+	modules: Vec<config::Value>,
+
+}
+
+impl McuManifest
+{
+
+	pub fn from_config(conf: &mut config::Config) -> McuManifest
+	{
+		McuManifest
+		{
+			core_deps: conf.get_table("EP_DEPS").unwrap_or_default(),
+			drivers: conf.get_array("DRIVERS").unwrap_or_default(),
+			modules: conf.get_array("MODULES").unwrap_or_default(),
+		}
+	}
+}
+
+impl std::fmt::Display for McuManifest
+{
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result
+	{
+		// print ep deps
+		write!(f, "Printing core dependencies:\n").unwrap();
+		for dep in &self.core_deps
+		{
+			write!(f, "{}: {}\n", &dep.0, &dep.1).unwrap();
+		}
+
+		// Available drivers
+		write!(f, "\nAvailable drivers:\n").unwrap();
+		for driver in &self.drivers
+		{
+			write!(f, "{}\n", &driver).unwrap();
+		}
+
+		// Available modules
+		write!(f, "\nAvailable modules:\n").unwrap();
+		for module in &self.modules
+		{
+			write!(f, "{}\n", &module).unwrap();
+		}
+		Ok(())
+	}
+}
+
+
