3bcc8b90b137797357240534022de49eedeb21c1
Penguin <penguin@epenguin.net>
2022-03-17 14:28:28 -0500

    .

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,36 @@
 // #![allow(warnings)]
-#![allow(non_snake_case)]
-#![allow(non_camel_case_types)]
-
-use igloo_core::Igloo;
-
-#[macro_use] extern crate igloo_util;
-use igloo_util::IglooDebugSeverity::{self, *};
-use igloo_util::IglooStatus::{self, *};
-use igloo_util::IglooType::{self, *};
-use igloo_util::TRACE_LEVEL;
+// #![allow(non_snake_case)]
+// #![allow(non_camel_case_types)]
+//
+// use igloo_core::Igloo;
+//
+// #[macro_use] extern crate igloo_util;
+// use igloo_util::IglooDebugSeverity::{self, *};
+// use igloo_util::IglooStatus::{self, *};
+// use igloo_util::IglooType::{self, *};
+// use igloo_util::TRACE_LEVEL;
+//
+// fn main()
+// {
+// 	let mut ig = Igloo::new();
+// 	let _start_ret = match ig.start()
+// 	{
+// 		Ok(it) =>
+// 		{
+// 			match ig.run(it)
+// 			{
+//
+// 				IS_GOOD => (),
+// 				// this is actually so dumb and should not exist in any language
+// 				ANYTHING_ELSE => igloo_debug!(ERROR, ANYTHING_ELSE),
+// 			}
+// 		}
+// 		Err(e) => println!("Error: {:?}", e),
+// 	};
+//
+// }
 
 fn main()
 {
-	let mut ig = Igloo::new();
-	let _start_ret = match ig.start()
-	{
-		Ok(it) =>
-		{
-			match ig.run(it)
-			{
-
-				IS_GOOD => (),
-				// this is actually so dumb and should not exist in any language
-				ANYTHING_ELSE => igloo_debug!(ERROR, ANYTHING_ELSE),
-			}
-		}
-		Err(e) => println!("Error: {:?}", e),
-	};
 
 }
